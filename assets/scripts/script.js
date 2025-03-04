022cbd631e4a28181c6ec30146c4cb1caa7e0b23
fsfuseini <suhuyini86@gmail.com>
2025-03-04 01:55:27 +0000

    Today's work

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,13 +8,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Hamburger Menu
+// Hamburger Menu Functionality
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
+// Toggle Hamburger Menu
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
 });
+
+// Close Hamburger Menu when a link is clicked
+navLinks.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+    hamburger.classList.remove('active');
+})
 
 // Newsletter Form Submission
 document.getElementById('newsletter-form').addEventListener('submit', function (e) {
