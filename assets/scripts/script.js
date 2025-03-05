10a229fee902dee32cc30052b2d2a4065858ccb1
fsfuseini <suhuyini86@gmail.com>
2025-03-05 05:47:55 +0000

    More styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -24,6 +24,17 @@ navLinks.addEventListener('click', () => {
     hamburger.classList.remove('active');
 })
 
+// Navbar Scroll Effect
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { // Adjust the scroll threshold as needed
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
 // Newsletter Form Submission
 document.getElementById('newsletter-form').addEventListener('submit', function (e) {
     e.preventDefault();
